2626617b0ad73be2aa145ca7cfa7dcc8cf8b4c5d
yulinchen7116 <r14227116@ntu.edu.tw>
2025-11-03 13:54:44 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/',
+
+  base: '/yulinchen7116-lotusbf.github.io/',
 
   plugins: [react()],
   resolve: {
